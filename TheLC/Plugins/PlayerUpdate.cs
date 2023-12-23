df949b808d0ee1def4bb1f48ebdb8a2de5369b24
Eric Davin <edavin@learn21.org>
2023-12-23 13:24:20 -0500

    Abstract away code from PluginMain because it makes my eyes happy

--- TheLC/Plugins/PlayerUpdate.cs
+++ TheLC/Plugins/PlayerUpdate.cs
@@ -1,10 +1,5 @@
 ﻿using GameNetcodeStuff;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace TheLC.Plugins
@@ -12,22 +7,90 @@ namespace TheLC.Plugins
     [HarmonyPatch(typeof(PlayerControllerB))]
     public sealed class PlayerUpdate
     {
+        public static float targetFov = 66f;
+        public static bool hideVisor;
+        public static float preFov = 0f;
+        private static Vector3 visorScale;
+        public static Vector3 visorScaleBottom = new Vector3(0.68f, 0.8f, 0.95f);
+        public static Vector3 visorScaleTop = new Vector3(0.68f, 0.35f, 0.99f);
+        public static float linToSinLerp = 0.6f;
+        public static float visorScaleTopRefFOV = 130f;
+
+
         [HarmonyPatch("Update")]
         [HarmonyPrefix]
-        public static void PrePatchUpdate(PlayerControllerB ___instance)
+        public static void PrePatchUpdate(PlayerControllerB __instance)
         {
-
+            preFov = __instance.gameplayCamera.fieldOfView;
+            if (__instance.localVisor.localScale != visorScale)
+            {
+                __instance.localVisor.localScale = visorScale;
+            }
         }
 
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
-        public static void PostPatchUpdate(PlayerControllerB ___instance ) 
+        public static void PostPatchUpdate(PlayerControllerB __instance) 
         {
-            ___instance.sprintMeter = ModSettings.InfiniteStaminaConfig.Value == true ? 1f : ___instance.sprintMeter;
-            ___instance.jumpForce = ModSettings.JumpHeightConfig.Value;
-            ___instance.movementSpeed = ModSettings.MovementSpeedConfig.Value;
-            ___instance.climbSpeed = ModSettings.ClimbSpeedConfig.Value;
-            ___instance.grabDistance = ModSettings.GrabDistanceConfig.Value;
+            __instance.sprintMeter = ModSettings.InfiniteStaminaConfig.Value == true ? 1f : __instance.sprintMeter;
+            __instance.jumpForce = ModSettings.JumpHeightConfig.Value;
+            __instance.movementSpeed = ModSettings.MovementSpeedConfig.Value;
+            __instance.climbSpeed = ModSettings.ClimbSpeedConfig.Value;
+            __instance.grabDistance = ModSettings.GrabDistanceConfig.Value;
+            __instance.UpdatePlayerFOV();
+        }
+
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        public static void PostPatchLateUpdate(PlayerControllerB __instance)
+        {
+            if (targetFov > 66f)
+            {
+                __instance.localVisor.position = __instance.localVisor.position + __instance.localVisor.rotation * new Vector3(0f, 0f, -0.06f);
+            }
+        }
+
+        public static void CalculateVisorScale()
+        {
+            if (hideVisor)
+            {
+                visorScale = new Vector3(0f, 0f, 0f);
+            }
+            else if (targetFov > 66f)
+            {
+                float num = (targetFov - 66f) / (visorScaleTopRefFOV - 66f);
+                num = Mathf.Lerp(num, EaseOutSine(num), linToSinLerp);
+                visorScale = Vector3.LerpUnclamped(visorScaleBottom, visorScaleTop, num);
+            }
+            else
+            {
+                visorScale = new Vector3(0.36f, 0.49f, 0.49f);
+            }
+        }
+        public static float EaseOutSine(float x) => Mathf.Sin(x * 3.1415927f / 2f);
+    }
+
+    public static class PlayerUpdateExtentions
+    {
+        public static PlayerControllerB UpdatePlayerFOV(this PlayerControllerB instance)
+        {
+            float num = PlayerUpdate.targetFov;
+            if (instance.inTerminalMenu)
+            {
+                num = 60f;
+            }
+            else if (instance.IsInspectingItem)
+            {
+                num = 46f;
+            }
+            else if (instance.isSprinting)
+            {
+                num *= 1.03f;
+            }
+            instance.gameplayCamera.fieldOfView = Mathf.Lerp(PlayerUpdate.preFov, num, 6f * Time.deltaTime);
+
+            return instance;
         }
     }
+
 }
---
+++ TheLC/SettingsHandler.cs
@@ -0,0 +1,113 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheLC.Plugins;
+using UnityEngine;
+
+namespace TheLC
+{
+    internal static class SettingsHandler
+    {
+
+        public static void RegisterSettings(ConfigFile Config)
+        {
+            ModSettings.InfiniteStaminaConfig = Config.Bind(ModSettingCategories.General, ModSettings.InfiniteStaminaKey, false, new ConfigDescription("Infinite stamina", new AcceptableValueRange<bool>(false, true)));
+
+            ModSettings.JumpHeightConfig = Config.Bind(ModSettingCategories.General, ModSettings.JumpHeightKey, 10f, new ConfigDescription("Set jump height", new AcceptableValueRange<float>(1f, 200f)));
+
+            ModSettings.MovementSpeedConfig = Config.Bind(ModSettingCategories.General, ModSettings.MovementSpeedKey, 5f, new ConfigDescription("Set movement speed", new AcceptableValueRange<float>(0f, 100f)));
+
+            ModSettings.ClimbSpeedConfig = Config.Bind(ModSettingCategories.General, ModSettings.ClimbSpeedKey, 4f, new ConfigDescription("Set climb speed", new AcceptableValueRange<float>(0f, 100f)));
+
+            ModSettings.ThrowPowerConfig = Config.Bind(ModSettingCategories.General, ModSettings.ThrowPowerKey, 17f, new ConfigDescription("Set throw power", new AcceptableValueRange<float>(0f, 100f)));
+
+            ModSettings.GrabDistanceConfig = Config.Bind(ModSettingCategories.General, ModSettings.GrabDistanceKey, 5f, new ConfigDescription("Set grab distance", new AcceptableValueRange<float>(0f, 100f)));
+
+            ModSettings.FOVConfig = Config.Bind(ModSettingCategories.Video, ModSettings.FOVKey, 66f, new ConfigDescription("Set FOV", new AcceptableValueRange<float>(66f, 130f)));
+
+            ModSettings.HideVisorConfig = Config.Bind(ModSettingCategories.Video, ModSettings.HideVisorKey, false, "Changes visor visability");
+
+            RegisterSettingEvents();
+        }
+
+        //This might come back to bit me 
+        private static void ConfigSettingChanged(object sender, System.EventArgs e)
+        {
+            SettingChangedEventArgs settingChangedEventArgs = e as SettingChangedEventArgs;
+
+            if (settingChangedEventArgs == null)
+                return;
+
+            if (settingChangedEventArgs.ChangedSetting.Definition.Key == ModSettings.FOVKey)
+            {
+                PlayerUpdate.targetFov = Mathf.Clamp(ModSettings.FOVConfig.Value, 66f, 130f);
+                PlayerUpdate.CalculateVisorScale();
+            }
+
+            if (settingChangedEventArgs.ChangedSetting.Definition.Key == ModSettings.HideVisorKey)
+            {
+                PlayerUpdate.hideVisor = ModSettings.HideVisorConfig.Value;
+                PlayerUpdate.CalculateVisorScale();
+            }
+
+            //if (settingChangedEventArgs.ChangedSetting.Definition.Key == KeyboardShortcutExampleKey)
+            //{
+            //    KeyboardShortcut newValue = (KeyboardShortcut)settingChangedEventArgs.ChangedSetting.BoxedValue;
+            //}
+        }
+
+        public static void SetInitalValues()
+        {
+            PlayerUpdate.targetFov = Mathf.Clamp(ModSettings.FOVConfig.Value, 66f, 130f);
+            PlayerUpdate.hideVisor = ModSettings.HideVisorConfig.Value;
+            PlayerUpdate.CalculateVisorScale();
+        }
+
+        private static void RegisterSettingEvents()
+        {
+            //Register settings changed event
+            ModSettings.FOVConfig.SettingChanged += ConfigSettingChanged;
+            ModSettings.HideVisorConfig.SettingChanged += ConfigSettingChanged;
+        }
+    }
+
+    public static class ModSettings
+    {
+        public static string InfiniteStaminaKey = "Infinite Stamina";
+        public static string JumpHeightKey = "Jump Height";
+        public static string MovementSpeedKey = "Movement Speed";
+        public static string ClimbSpeedKey = "Climb Speed";
+        public static string ThrowPowerKey = "Throw Item/Player Power";
+        public static string GrabDistanceKey = "Item/Player Grab Distance";
+        public static string FOVKey = "Player FOV";
+        public static string HideVisorKey = "Hide Visor";
+
+        //public static string KeyboardShortcutExampleKey = "Recall Keyboard Shortcut";
+
+        public static ConfigEntry<bool> InfiniteStaminaConfig;
+        public static ConfigEntry<float> JumpHeightConfig;
+        public static ConfigEntry<float> MovementSpeedConfig;
+        public static ConfigEntry<float> ClimbSpeedConfig;
+        public static ConfigEntry<float> ThrowPowerConfig;
+        public static ConfigEntry<float> GrabDistanceConfig;
+        public static ConfigEntry<float> FOVConfig;
+        public static ConfigEntry<bool> HideVisorConfig;
+        //public static ConfigEntry<KeyboardShortcut> KeyboardShortcutExample;
+    }
+
+    public static class ModSettingCategories
+    {
+        public const string General = "General";
+        public const string Video = "Video";
+    }
+
+    public static class ModConstraints
+    {
+        public const string modGuid = "Peanut.TheLC";
+        public const string modName = "TheLC";
+        public const string modVersion = "1.0.0";
+    }
+}
